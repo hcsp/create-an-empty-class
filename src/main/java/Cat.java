829fc0823e94c3f79b798330d4d469f32f0f57ed
zhuxueqiao <mya_dong@126.com>
2020-03-02 00:06:05 +0800

    类

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,5 +1,6 @@
 public class Cat {
-    public static void main(String[] args) {
-        System.out.println("Hello Word");
+    public static void main(String[] args){
+        System.out.println("Hello world!");
     }
+
 }
